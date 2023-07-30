fe02e96919765305e9dcd6d6680ae5b94e77f987
A.B. Rakib <96842069+abrakib942@users.noreply.github.com>
2023-07-30 17:27:27 +0600

    private route

---
+++ src/pages/pcBuild.js
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const PcBuild = () => {
+  return (
+    <div>
+      <Navbar />
+      Pc builder
+    </div>
+  );
+};
+
+export default PcBuild;
